7a198a7d793804db510016f4e76919b68448cb54
mbleda <martableda@gmail.com>
2013-10-10 12:47:26 +0200

    Class 3 files added

---
+++ classes/class3/myFunctions.R
@@ -0,0 +1,32 @@
+###############################################
+## Corrected degree distribution function (R 3.0)
+###############################################
+my.degree.distribution <- function(graph, cumulative = FALSE, ...){
+  if (!is.igraph(graph)) {
+    stop("Not a graph object")
+  }
+  cs <- degree(graph, ...)
+  hi <- hist(cs, -1:max(cs), plot = FALSE)$density
+  if (!cumulative) {
+    res <- hi
+  }
+  else {
+    res <- rev(cumsum(rev(hi)))
+  }
+  res
+}
+
+my.count.degree.distribution <- function(graph, cumulative = FALSE, ...){
+  if (!is.igraph(graph)) {
+    stop("Not a graph object")
+  }
+  cs <- degree(graph, ...)
+  hi <- hist(cs, -1:max(cs), plot = FALSE)$count
+  if (!cumulative) {
+    res <- hi
+  }
+  else {
+    res <- rev(cumsum(rev(hi)))
+  }
+  res
+}
